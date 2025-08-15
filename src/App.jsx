2664dab61fc6f23c6d5e9df9fbb34cb2f51451e1
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-15 05:29:58 +0200

    desktop layout fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Card from "./Card";
 import { cardData } from "./data/data";
 function App() {
   return (
-    <main className="min-h-screen py-20 px-10 bg-Purple-50 font-barlow">
-      <section className="">
+    <main className="min-h-screen py-22 px-10 bg-Purple-50 font-barlow md:flex md:items-center md:justify-center">
+      <section className=" md:grid md:grid-cols-4 md:grid-rows-2 md:gap-1  md:w-9/10 ">
         {cardData.map((item, index) => (
           <Card props={item} index={index} key={index} />
         ))}
--- src/Card.jsx
+++ src/Card.jsx
@@ -8,24 +8,38 @@ export default function Card({ props, index }) {
     "bg-White text-Grey-500",
   ];
 
+  const gridPosition = [
+    "md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-2 md:bg-[url('/public/bg-pattern-quotation.svg')] bg-no-repeat bg-position-(--custom-bg-position)",
+    "md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-2",
+    "md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3",
+    "md:col-start-2 md:col-end-4 md:row-start-2 md:row-end-3",
+    "md:col-start-4 md:col-end-5 md:row-start-1 md:row-end-3",
+  ];
+
   return (
     <article
-      className={`flex flex-col gap-4 mb-10 last-of-type:mb-0 p-8 rounded-xl  ${backgroundColors[index]}`}
+      className={`grid grid-cols-[45px_auto] grid-rows-[16px_16px_auto_minmax(auto,1fr)] py-8 px-7 m-4 mb-12 md:m-4 rounded-xl shadow-[35px_50px_20px_5px_rgba(0,_0,_0,_0.15)] ${backgroundColors[index]} ${gridPosition[index]} `}
       key={index}
     >
-      <div className="flex items-center gap-4">
-        <img
-          className="rounded-full border-1 border-Purple-50 w-12 h-12"
-          src={image}
-          alt={`An image of ${name}`}
-        />
-        <div className="flex flex-col">
-          <h2 className="font-semibold">{name}</h2>
-          <p className="font-normal">{occupation}</p>
-        </div>
-      </div>
-      <p className="font-medium text-2xl">{summary}</p>
-      <p className="">{quote}</p>
+      <img
+        className="h-10 w-auto rounded-full border-1 border-Purple-50 col-start-1 col-end-2 row-start-1 row-end-3 "
+        src={image}
+        alt={`An image of ${name}`}
+      />
+
+      <h2 className="font-medium text-sm col-start-2 col-end-3 row-start-1 row-end-2">
+        {name}
+      </h2>
+      <p className="font-normal text-xs mt-0.5 col-start-2 col-end-3 row-start-2 row-end-3">
+        {occupation}
+      </p>
+
+      <p className="font-medium text-xl my-4 col-start-1 col-end-3 row-start-3 row-end-4">
+        {summary}
+      </p>
+      <p className="text-sm/1.6 line-h col-start-1 col-end-3 row-start-4 row-end-5">
+        {quote}
+      </p>
     </article>
   );
 }
